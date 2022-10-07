4db4715b1bcd9f520ec567605ceb1a96f04422b4
Jagdeep Singh <jdecode@gmail.com>
2022-10-08 00:40:33 +0530

    No init/new-main, push -f main:gh-pages

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,13 @@ git config user.email "jdecode@gmail.com"
 git config user.username "jdecode"
 git config user.name "Jagdeep Singh"
 git remote -v
-git status
-git init
-git checkout -b main
+git statuss
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 #git push -f git@github.com:indiatalks-dev/indiatalks-dev.github.io.git main
-git push -f https://jdecode:${{INDIA_TALKS_DEV_PAGES_DEPLOY_TOKEN}}@github.com/indiatalks-dev/indiatalks-dev.git main
+git push -f https://jdecode:${{INDIA_TALKS_DEV_PAGES_DEPLOY_TOKEN}}@github.com/indiatalks-dev/indiatalks-dev.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
